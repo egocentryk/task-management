78eec4716cf71b3539130b07ae8bfad27c0a2c1a
egocentryk <lukasz.skowron@gmail.com>
2025-02-12 11:25:05 +0100

    feat(): typeorm & pg installed, db connection established

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { TasksModule } from './components/tasks/tasks.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
