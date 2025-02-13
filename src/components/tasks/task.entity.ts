93e4584174aa587d93e7de671fddcac4ef063f8c
egocentryk <lukasz.skowron@gmail.com>
2025-02-13 10:16:42 +0100

    feat(): task repository created

--- src/components/tasks/task.entity.ts
+++ src/components/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 import { TASK_STATUS } from './task.model'
 import { ValueOf } from 'src/common/types/types'
+import { IsNotEmpty } from 'class-validator'
 
 @Entity()
 export class Task {
@@ -8,11 +9,16 @@ export class Task {
   id: string
 
   @Column()
+  @IsNotEmpty()
   title: string
 
   @Column()
   description: string
 
-  @Column()
+  @Column({
+    default: TASK_STATUS.OPEN,
+    type: 'enum',
+    enum: TASK_STATUS,
+  })
   status: ValueOf<typeof TASK_STATUS>
 }
