2e8563a4acf22964581667eeae4d11416a0c7726
egocentryk <lukasz.skowron@gmail.com>
2025-02-16 18:21:58 +0100

    fea(): one to many & many to one relations implemented

--- src/components/tasks/task.entity.ts
+++ src/components/tasks/task.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 import { TASK_STATUS } from './task.model'
 import { ValueOf } from 'src/common/types/types'
 import { IsNotEmpty } from 'class-validator'
+import { User } from 'src/auth/user.entity'
 
 @Entity()
 export class Task {
@@ -21,4 +22,7 @@ export class Task {
     enum: TASK_STATUS,
   })
   status: ValueOf<typeof TASK_STATUS>
+
+  @ManyToOne((_user) => User, (user) => user.tasks, { eager: false })
+  user: User
 }
