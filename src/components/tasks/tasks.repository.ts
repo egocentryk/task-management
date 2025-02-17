09b979d4ab00eb5312f856b8938caf6d9c9a55c6
egocentryk <lukasz.skowron@gmail.com>
2025-02-17 11:05:27 +0100

    feat(): logger implemented, examples created

--- src/components/tasks/tasks.repository.ts
+++ src/components/tasks/tasks.repository.ts
@@ -1,12 +1,16 @@
 import { DataSource, Repository } from 'typeorm'
 import { Task } from './task.entity'
-import { Injectable } from '@nestjs/common'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
 import { CreateTaskDto } from './dto/create-task.dto'
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
 import { User } from 'src/auth/user.entity'
+import { Logger } from '@nestjs/common'
+import { time } from 'console'
 
 @Injectable()
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TasksRepository', { timestamp: true })
+
   constructor(private dataSource: DataSource) {
     super(Task, dataSource.createEntityManager())
   }
@@ -28,8 +32,18 @@ export class TasksRepository extends Repository<Task> {
       )
     }
 
-    const tasks = await query.getMany()
-    return tasks
+    try {
+      const tasks = await query.getMany()
+      return tasks
+    } catch (error) {
+      this.logger.error(
+        `Failed to get tasks for user "${user.username}". Filters: ${JSON.stringify(
+          filterDto,
+        )}`,
+        error.stack,
+      )
+      throw new InternalServerErrorException()
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
